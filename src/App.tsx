ce77d313787f261a7c36ea3dc9c73c625cb2b502
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-02-28 01:00:42 +0300

    Implementing styles and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,90 @@
-// import ListGroup from './components/ListGroup';
+// // import ListGroup from './components/ListGroup';
 
-import { useState } from 'react';
-import Alert from './components/Alert';
-import Button from './components/Button';
+// import { useState } from 'react';
+// import Alert from './components/Alert';
+// import Button from './components/Button';
 
-function App() {
-  // let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+// function App() {
+//   // let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
 
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // };
-  // return (
-  //   <div>
-  //     <ListGroup
-  //       items={items}
-  //       heading="Cities"
-  //       onSelectItem={handleSelectItem}
-  //     />
-  //   </div>
-  // );
+//   // const handleSelectItem = (item: string) => {
+//   //   console.log(item);
+//   // };
+//   // return (
+//   //   <div>
+//   //     <ListGroup
+//   //       items={items}
+//   //       heading="Cities"
+//   //       onSelectItem={handleSelectItem}
+//   //     />
+//   //   </div>
+//   // );
 
-  // return (
-  //   <div>
-  //     <Alert>
-  //       Hello <span>World</span>
-  //     </Alert>
-  //   </div>
-  // );
+//   // return (
+//   //   <div>
+//   //     <Alert>
+//   //       Hello <span>World</span>
+//   //     </Alert>
+//   //   </div>
+//   // );
 
-  const [alertVisible, setAlerVisibility] = useState<boolean>(false);
+//   const [alertVisible, setAlerVisibility] = useState<boolean>(false);
+
+//   return (
+//     <div>
+//       {alertVisible && (
+//         <Alert onClose={() => setAlerVisibility(false)}>My Alert</Alert>
+//       )}
+//       <Button color="primary" onClick={() => setAlerVisibility(true)}>
+//         My Button
+//       </Button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+/******************************************************************************************************************/
+
+// import ListGroup from "./components/ListGroup";
+
+// const App = ()=>{
+//   const items = ["NY", "LA","SF"];
+
+//   return (
+//     <div>
+//       <ListGroup heading="Miami" items={items} onSelectItem={()=>{}}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/******************************************************************************************************************/
+
+// import Icon from "./components/Icons";
+
+// const App= ()=>{
+
+//   return (
+//     <div>
+//       <Icon/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/******************************************************************************************************************/
+
+import Like from "./components/Like";
+
+const App= ()=>{
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlerVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="primary" onClick={() => setAlerVisibility(true)}>
-        My Button
-      </Button>
+      <Like onClick={()=>console.log("clicked")}/>
     </div>
   );
 }
