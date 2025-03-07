9570c20f2be57b06b729c86c83e14ae783aeff64
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-08 00:35:33 +0300

    Implementing Expense List Component

--- src/App.tsx
+++ src/App.tsx
@@ -382,13 +382,36 @@ import Form from './components/Form';
 
 /******************************************************************************************************************/
 
-import './components/index.css';
-function App() {
+// import './components/index.css';
+// function App() {
+//   return (
+//     <div>
+//       <Form />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/******************************************************************************************************************/
+
+import React from 'react'
+import ExpenseList from './expense-tracker/components/ExpenseList';
+
+const App = () => {
+  const [expenses, setExpenses]=useState([
+    {id:1, description:'aaa', amount:10, category: 'Utilities'},
+    {id:2, description:'bbb', amount:10, category: 'Utilities'},
+    {id:3, description:'ccc', amount:10, category: 'Utilities'},
+    {id:4, description:'fff', amount:10, category: 'Utilities'},
+  ])
+
   return (
     <div>
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={(id)=> setExpenses(expenses.filter(e=>e.id != id))}></ExpenseList>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
