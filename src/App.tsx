c9a22c0a2721330e418d5ec8e058512f58f86ff2
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-08 01:24:26 +0300

    Implementing Expense Filter Component

--- src/App.tsx
+++ src/App.tsx
@@ -393,25 +393,53 @@ import Form from './components/Form';
 
 // export default App;
 
+// /******************************************************************************************************************/
+
+// import React from 'react'
+// import ExpenseList from './expense-tracker/components/ExpenseList';
+
+// const App = () => {
+//   const [expenses, setExpenses]=useState([
+//     {id:1, description:'aaa', amount:10, category: 'Utilities'},
+//     {id:2, description:'bbb', amount:10, category: 'Utilities'},
+//     {id:3, description:'ccc', amount:10, category: 'Utilities'},
+//     {id:4, description:'fff', amount:10, category: 'Utilities'},
+//   ])
+
+//   return (
+//     <div>
+//       <ExpenseList expenses={expenses} onDelete={(id)=> setExpenses(expenses.filter(e=>e.id != id))}></ExpenseList>
+//     </div>
+//   )
+// }
+
+// export default App
+
 /******************************************************************************************************************/
 
 import React from 'react'
 import ExpenseList from './expense-tracker/components/ExpenseList';
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory]= useState('');
+
   const [expenses, setExpenses]=useState([
     {id:1, description:'aaa', amount:10, category: 'Utilities'},
-    {id:2, description:'bbb', amount:10, category: 'Utilities'},
-    {id:3, description:'ccc', amount:10, category: 'Utilities'},
+    {id:2, description:'bbb', amount:10, category: 'Entertainment'},
+    {id:3, description:'ccc', amount:10, category: 'Groceries'},
     {id:4, description:'fff', amount:10, category: 'Utilities'},
   ])
 
+  const visibleExpenses = selectedCategory? expenses.filter(e=>e.category=== selectedCategory): expenses;
   return (
     <div>
-      <ExpenseList expenses={expenses} onDelete={(id)=> setExpenses(expenses.filter(e=>e.id != id))}></ExpenseList>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={category=> setSelectedCategory(category)}/>
+      </div>
+      <ExpenseList expenses={visibleExpenses} onDelete={(id)=> setExpenses(expenses.filter(e=>e.id != id))}></ExpenseList>
     </div>
   )
 }
 
 export default App
-
