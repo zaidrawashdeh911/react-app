c2d96820aaac0608c744debf182c355a865ab94b
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-12 01:05:49 +0300

    Implementing Update Data

--- src/App.tsx
+++ src/App.tsx
@@ -760,7 +760,91 @@ import Form from './components/Form';
 /******************************************************************************************************************/
 // creating data
 
+// import axios, { CanceledError } from 'axios';
+
+// interface User{
+//   id:number;
+//   name:string;
+// }
+// const App = () => {
+//   const [users,setUsers] = useState<User[]>([]);
+//   const [error, setError]= useState('');
+//   const [isLoading, setLoading] = useState(false);
+
+//   useEffect(()=>{
+//     const controller = new AbortController();
+
+//     setLoading(true);
+
+//     // get returns a promise, if it's resolved we get a response object else we get an error
+//     axios
+//       .get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
+//       .then(res=>{
+//         setUsers(res.data)
+//         setLoading(false);
+//         // order isn't importnant because jsx renders the component once
+//       })
+//       .catch(err=>{
+//         if(err instanceof CanceledError) return;
+//         setLoading(false);
+//         setError(err.message);
+//       });
+
+//       return ()=> controller.abort();
+//   },[]);
+
+//   const deleteUser = (user:User)=>{
+//     const originalUsers = [...users];
+//     setUsers(users.filter(u => u.id !== user.id));
+//     axios.delete('https://jsonplaceholder.typicode.com/users/' + user.id)
+//     .catch(err=>{
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     })
+//   }
+
+//   const addUser= ()=>{
+//     const originalUsers = [...users];
+//     const newUser= {id:0, name:'Mosh'};
+//     setUsers([newUser,...users]);
+
+//     axios.post('https://jsonplaceholder.typicode.com/users', newUser)
+//     // .then(res=> setUsers([res.data,...users]));
+//     // another way is to destructure the response
+//     // savedUser is just an alias for data object to make the code more readable
+//     .then(({data: savedUser})=> setUsers([savedUser,...users]))
+//     .catch(err=>{
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     })
+//   }
+
+//   return (
+//   <>
+//   {/* this logic renders the html element only if the variable before is true */}
+//     {error && <p className="text-danger">{error}</p>}
+//     {isLoading && <div className="spinner-boarder"></div>}
+
+//     <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+//     <ul className='list-group'>
+//       {users.map(user=>
+//       <li key={user.id} className='list-group-item d-flex justify-content-between'>
+//         {user.name}
+//       <button className="btn btn-outline-danger" onClick={()=> deleteUser(user)}>Delete</button>
+//       </li>)}
+//     </ul>
+//   </>
+//   );
+// }
+
+// export default App;
+
+/******************************************************************************************************************/
+
+//Updating Data
+
 import axios, { CanceledError } from 'axios';
+import { useForm } from 'react-hook-form';
 
 interface User{
   id:number;
@@ -819,6 +903,20 @@ const App = () => {
     })
   }
 
+  const updateUser = (user:User)=>{
+    const originalUsers = [...users];
+
+    const updatedUser = {...user, name:user.name + ' !'}
+    setUsers(users.map(u=> u.id === user.id? updatedUser: u))
+
+    axios
+      .patch('https://jsonplaceholder.typicode.com/users/'+ user.id, updatedUser)
+      .catch(err=>{
+        setError(err.message);
+        setUsers(originalUsers);
+      })
+  }
+
   return (
   <>
   {/* this logic renders the html element only if the variable before is true */}
@@ -830,7 +928,10 @@ const App = () => {
       {users.map(user=>
       <li key={user.id} className='list-group-item d-flex justify-content-between'>
         {user.name}
-      <button className="btn btn-outline-danger" onClick={()=> deleteUser(user)}>Delete</button>
+        <div>
+          <button className="btn btn-outline-secondary mx-1" onClick={()=> updateUser(user)}>Update</button>
+          <button className="btn btn-outline-danger" onClick={()=> deleteUser(user)}>Delete</button>
+        </div>
       </li>)}
     </ul>
   </>
@@ -838,7 +939,3 @@ const App = () => {
 }
 
 export default App;
-
-/******************************************************************************************************************/
-
-//Updating Data
