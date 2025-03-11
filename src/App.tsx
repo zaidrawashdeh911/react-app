c0d98659e38bd2a476fe0d6a3e9c9e3acfb3fe04
zaidrawashdeh911 <zai20200907@std.psut.edu.jo>
2025-03-12 02:18:15 +0300

    Implementing a custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1038,31 +1038,103 @@ import Form from './components/Form';
 
 /******************************************************************************************************************/
 
-import { CanceledError} from './services/api-client';
+// import { CanceledError} from './services/api-client';
+// import userService, { User } from './services/user-service';
+
+// const App = () => {
+//   const [users,setUsers] = useState<User[]>([]);
+//   const [error, setError]= useState('');
+//   const [isLoading, setLoading] = useState(false);
+
+//   useEffect(()=>{
+//     setLoading(true);
+//     // get returns a promise, if it's resolved we get a response object else we get an error
+//     const {request, cancel}= userService.getAll<User>()
+//       request.then(res=>{
+//         setUsers(res.data)
+//         setLoading(false);
+//         // order isn't importnant because jsx renders the component once
+//       })
+//       .catch(err=>{
+//         if(err instanceof CanceledError) return;
+//         setLoading(false);
+//         setError(err.message);
+//       });
+
+//       return ()=> cancel();
+//   },[]);
+
+//   const deleteUser = (user:User)=>{
+//     const originalUsers = [...users];
+//     setUsers(users.filter(u => u.id !== user.id));
+//     userService.delete(user.id)
+//     .catch(err=>{
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     })
+//   }
+
+//   const addUser= ()=>{
+//     const originalUsers = [...users];
+//     const newUser= {id:0, name:'Mosh'};
+//     setUsers([newUser,...users]);
+
+//     userService.create(newUser)
+//     // .then(res=> setUsers([res.data,...users]));
+//     // another way is to destructure the response
+//     // savedUser is just an alias for data object to make the code more readable
+//     .then(({data: savedUser})=> setUsers([...users, savedUser]))
+//     .catch(err=>{
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     })
+//   }
+
+//   const updateUser = (user:User)=>{
+//     const originalUsers = [...users];
+
+//     const updatedUser = {...user, name:user.name + ' !'}
+//     setUsers(users.map(u=> u.id === user.id? updatedUser: u))
+//     userService
+//     .update(updatedUser)
+//     .catch(err=>{
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     })
+//   }
+
+//   return (
+//   <>
+//   {/* this logic renders the html element only if the variable before is true */}
+//     {error && <p className="text-danger">{error}</p>}
+//     {isLoading && <div className="spinner-boarder"></div>}
+
+//     <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+//     <ul className='list-group'>
+//       {users.map(user=>
+//       <li key={user.id} className='list-group-item d-flex justify-content-between'>
+//         {user.name}
+//         <div>
+//           <button className="btn btn-outline-secondary mx-1" onClick={()=> updateUser(user)}>Update</button>
+//           <button className="btn btn-outline-danger" onClick={()=> deleteUser(user)}>Delete</button>
+//         </div>
+//       </li>)}
+//     </ul>
+//   </>
+//   );
+// }
+
+// export default App;
+
+/******************************************************************************************************************/
+
+//Create a custom hook
+
 import userService, { User } from './services/user-service';
+import useUsers from './hooks/useUsers';
 
 const App = () => {
-  const [users,setUsers] = useState<User[]>([]);
-  const [error, setError]= useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(()=>{
-    setLoading(true);
-    // get returns a promise, if it's resolved we get a response object else we get an error
-    const {request, cancel}= userService.getAll<User>()
-      request.then(res=>{
-        setUsers(res.data)
-        setLoading(false);
-        // order isn't importnant because jsx renders the component once
-      })
-      .catch(err=>{
-        if(err instanceof CanceledError) return;
-        setLoading(false);
-        setError(err.message);
-      });
-
-      return ()=> cancel();
-  },[]);
+  const {users, error, isLoading, setUsers, setError}=useUsers();
 
   const deleteUser = (user:User)=>{
     const originalUsers = [...users];
